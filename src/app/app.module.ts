26a39cdc63fcb64c4ec417db52719182515761cf
Charleston Campos <charlinhocjc@gmail.com>
2019-01-05 17:18:58 -0200

    css effects.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CharactersModule } from './characters/characters.module';
+import { HeaderModule } from './shared/components/header/header.module';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
+    HeaderModule,
     RouterModule,
     BrowserModule,
     CharactersModule,
