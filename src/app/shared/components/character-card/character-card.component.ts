53408bc1c4c93873dc4bb23f49f4ee87afda43f7
Charleston Campos <charlinhocjc@gmail.com>
2019-01-05 16:32:36 -0200

    bug fixed

--- src/app/shared/components/character-card/character-card.component.ts
+++ src/app/shared/components/character-card/character-card.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Character } from '@app/core/models/character.model';
 
 @Component({
   selector: 'co-ex-character-card',
   templateUrl: './character-card.component.html',
-  styleUrls: ['./character-card.component.scss']
+  styleUrls: ['./character-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CharacterCardComponent {
 
